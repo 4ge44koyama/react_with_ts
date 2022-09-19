926af0b8b993b6006246c73291697e1dcbce62fc
4ge44koyama <m220552a@gmail.com>
2022-09-19 21:37:27 +0900

    動くところまで

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChangeEvent, useState, FC } from 'react';
+import styled from 'styled-components';
 
-function App() {
+export const App: FC = () => {
+  // テキストボックスState
+  const [text, setText] = useState<string>('');
+  // メモ一覧State
+  const [memos, setMemos] = useState<string[]>([]);
+
+  // テキストボックス入力時に入力内容をStateに設定
+  const onChangeText = (e: ChangeEvent<HTMLInputElement>) => setText(e.target.value);
+
+  // [追加]ボタン押下時
+  const onClickAdd = () => {
+    // State変更を正常に検知させるため新たに配列を生成
+    const newMemos = [...memos];
+    // テキストボックスの入力内容をメモ配列に追加
+    newMemos.push(text);
+    setMemos(newMemos);
+    // テキストボックスを空に
+    setText('');
+  };
+
+  // [削除]ボタン押下時(何番目が押されたかを引数で受け取る)
+  const onClickDelete = (index: number) => {
+    // State変更を正常に検知させるため新たな配列を生成
+    const newMemos = [...memos];
+    // メモ配列から該当の要素を削除
+    newMemos.splice(index, 1);
+    setMemos(newMemos);
+  };
+
+  // スタイルここから
+  const SButton = styled.button`
+  margin-left: 16px;
+  `;
+
+  const SContainer = styled.div`
+    border: solid 1px #ccc;
+    padding: 16px;
+    margin: 8px;
+  `;
+
+  const SMemoWrapper = styled.div`
+    display: flex;
+    align-items: center;
+  `;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>簡単メモアプリ</h1>
+      <input type="text" value={text} onChange={onChangeText} />
+      <SButton onClick={onClickAdd}>追加</SButton>
+      <SContainer>
+        <p>メモ一覧</p>
+        <ul>
+          {memos.map((memo, index) => (
+            <li key={memo}>
+              <SMemoWrapper>
+                <p>{memo}</p>
+                <SButton onClick={() => {onClickDelete(index)}}>削除</SButton>
+              </SMemoWrapper>
+            </li>
+          ))}
+        </ul>
+      </SContainer>
     </div>
   );
-}
-
-export default App;
+};
